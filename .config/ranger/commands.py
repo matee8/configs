c54eced99d69c937564b14364b6fb0ced0a1d0ed
matee8 <molnarmatenorbert@gmail.com>
2024-04-01 22:07:06 +0200

    .

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -17,10 +17,6 @@ from ranger.api.commands import Command
 
 from ranger.core.loader import CommandLoader
 
-from ranger.core.loader import Loadable
-from ranger.ext.signals import SignalDispatcher
-from ranger.ext.shell_escape import *
-
 # Any class that is a subclass of "Command" will be integrated into ranger as a
 # command.  Try typing ":my_edit<ENTER>" in ranger!
 class my_edit(Command):
@@ -136,48 +132,3 @@ class compress(Command):
 
         extension = ['.zip', '.tar.gz', '.rar', '.7z']
         return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
-
-class MountLoader(Loadable, SignalDispatcher):
-    """
-    Wait until a directory is mounted
-    """
-    def __init__(self, path):
-        SignalDispatcher.__init__(self)
-        descr = "Waiting for dir '" + path + "' to be mounted"
-        Loadable.__init__(self, self.generate(), descr)
-        self.path = path
-
-    def generate(self):
-        available = False
-        while not available:
-            try:
-                if os.path.ismount(self.path):
-                    available = True
-            except:
-                pass
-            yield
-            time.sleep(0.03)
-        self.signal_emit('after')
-
-class mount(Command):
-    def execute(self):
-        selected_files = self.fm.thisdir.get_selection()
-
-        if not selected_files:
-            return
-
-        space = ' '
-        self.fm.execute_command("cdemu -b system unload 0")
-        self.fm.execute_command("cdemu -b system load 0 " + \
-                space.join([shell_escape(f.path) for f in selected_files]))
-
-        mountpath = "/media/virtualrom/"
-
-        def mount_finished(path):
-            currenttab = self.fm.current_tab
-            self.fm.tab_open(9, mountpath)
-            self.fm.tab_open(currenttab)
-
-        obj = MountLoader(mountpath)
-        obj.signal_bind('after', mount_finished)
-        self.fm.loader.add(obj)
