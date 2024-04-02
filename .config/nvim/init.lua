d2b2d5dff5f871613af183c4dfd6a0d0000a7def
matee8 <graves-bluff-pesky@duck.com>
2024-04-02 10:39:17 +0200

    .

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- Line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Indentation
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.autoindent = ai
+
+-- System clipboard
+vim.o.clipboard = 'unnamedplus'
+
+-- Disable mouse
+vim.o.mouse = nil
