60e4ccac29572a46c8dedc047470ef02f60716e8
matee8 <molnarmatenorbert@gmail.com>
2025-01-08 16:09:11 +0100

    .

--- home/mate/.config/nvim/lua/config/plugins/copilotchat.lua
+++ home/mate/.config/nvim/lua/config/plugins/copilotchat.lua
@@ -4,9 +4,9 @@ return {
     dependencies = {
         "github/copilot.vim",
     },
-    opts = { 
-        model = 'claude-3.5-sonnet',
-        agent = 'copilot',
+    opts = {
+        model = "claude-3.5-sonnet",
+        agent = "copilot",
         temperature = 1.0,
         window = {
             width = 0.3,
@@ -15,15 +15,19 @@ return {
         show_help = false,
         mappings = {
             close = {
-                insert = '<C-q>',
+                insert = "<C-q>",
+            },
+            reset = {
+                insert = "<C-r>",
+                normal = "<C-r>",
             },
             accept_diff = {
-                normal = '<C-f>',
-                insert = '<C-f>',
+                normal = "<C-f>",
+                insert = "<C-f>",
             },
-        },  
+        },
         highlight_headers = false,
-        separator = '---',
-        error_header = '> [!ERROR] Error',
+        separator = "---",
+        error_header = "> [!ERROR] Error",
     },
 }
--- home/mate/.config/nvim/lua/config/plugins/treesitter.lua
+++ home/mate/.config/nvim/lua/config/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     event = "VeryLazy",
-    build = "<CMD>TSUpdate<CR>",
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
