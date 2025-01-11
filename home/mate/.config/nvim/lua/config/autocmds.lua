b8c06e83a9323ed206cd03afea0d54ba188d82e3
matee8 <graves-bluff-pesky@duck.com>
2025-01-11 11:34:40 +0100

    .

--- home/mate/.config/nvim/lua/config/autocmds.lua
+++ home/mate/.config/nvim/lua/config/autocmds.lua
@@ -1,14 +1,12 @@
 local create_autocmd = vim.api.nvim_create_autocmd
 
--- Highlight yanked text
 create_autocmd("TextYankPost", {
     pattern = "*",
     command = "silent! lua vim.highlight.on_yank({ timeout =  500 })",
 })
 
--- Fix LSP floating window height and width
 create_autocmd("LspAttach", {
-    callback = function()
+    callback = function(opts)
         local orig_util_open_floating_preview =
             vim.lsp.util.open_floating_preview
         function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
@@ -16,6 +14,10 @@ create_autocmd("LspAttach", {
             opts.max_width = opts.max_width or 80
             opts.max_height = opts.max_height or 15
             return orig_util_open_floating_preview(contents, syntax, opts, ...)
+        end
+
+        if vim.bo[opts.buf].filetype == "markdown" then
+            require("otter").activate()
         end
     end,
 })
