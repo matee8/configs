cdec6b2ee1191e0b4fdbf2961f62c8c57f2ca5e7
matee8 <molnarmatenorbert@gmail.com>
2025-01-11 11:34:40 +0100

    .

--- home/mate/.config/nvim/lua/config/plugins/gitsigns.lua
+++ home/mate/.config/nvim/lua/config/plugins/gitsigns.lua
@@ -1,5 +1,11 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        on_attach = function(bufnr)
+            if vim.api.nvim_buf_get_name(bufnr):match("%.ipynb$") then
+                return false
+            end
+        end,
+    },
 }
