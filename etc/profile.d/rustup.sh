55c717497a6d7f629989dd0b2c5cfaabce49efbb
matee8 <graves-bluff-pesky@duck.com>
2025-04-29 21:28:44 +0200

    .

--- etc/profile.d/rustup.sh
+++ etc/profile.d/rustup.sh
@@ -1,6 +1,3 @@
 append_path '/usr/lib/rustup/bin'
-append_path "$XDG_DATA_HOME"/cargo/bin
+append_path $HOME/.cargo/bin
 export PATH
-
-export CARGO_HOME="$XDG_DATA_HOME"/cargo
-export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
