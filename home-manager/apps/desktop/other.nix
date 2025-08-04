b1847ba5c53d2a1fb6a61b50fe92669009560c5b
matee8 <graves-bluff-pesky@duck.com>
2025-08-04 19:55:43 +0200

    feat(home): add default applications

--- home-manager/apps/desktop/other.nix
+++ home-manager/apps/desktop/other.nix
@@ -7,6 +7,29 @@
 }:
 {
     config = lib.mkIf config.custom.home-manager.desktop.enable {
+        home.file.".local/share/applications/ffplay.desktop".source =
+            ../../../assets/applications/ffplay.desktop;
+
+        xdg.mimeApps = {
+            enable = true;
+            defaultApplications = {
+                "image/png" = "imv.desktop";
+                "image/jpeg" = "imv.desktop";
+                "image/gif" = "imv.desktop";
+                "image/bmp" = "imv.desktop";
+                "image/tiff" = "imv.desktop";
+                "video/mp4" = "ffplay.desktop";
+                "video/x-matroska" = "ffplay.desktop";
+                "audio/mpeg" = "ffplay.desktop";
+                "application/pdf" = "org.pwmt.zathura.desktop";
+                "text/html" = "librewolf.desktop";
+                "x-scheme-handler/http" = "librewolf.desktop";
+                "x-scheme-handler/https" = "librewolf.desktop";
+                "x-scheme-handler/about" = "librewolf.desktop";
+                "x-scheme-handler/unknown" = "librewolf.desktop";
+            };
+        };
+
         home.packages = with pkgs; [
             wl-clipboard
             wbg
