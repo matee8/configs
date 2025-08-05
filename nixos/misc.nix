134a1831ac1526eab43e65f91edfcd8b64c98c34
matee8 <graves-bluff-pesky@duck.com>
2025-08-05 16:11:07 +0200

    feat(nixos): add `misc.nix` for other configuration

---
+++ nixos/misc.nix
@@ -0,0 +1,44 @@
+{
+    lib,
+    config,
+    ...
+}:
+{
+    options = {
+        custom.timeZone = lib.mkOption {
+            type = lib.types.str;
+            default = "Europe/Budapest";
+        };
+        custom.locale = lib.mkOption {
+            type = lib.types.str;
+            default = "en_US.UTF-8";
+        };
+        custom.extraLocale = lib.mkOption {
+            type = lib.types.str;
+            default = "hu_HU.UTF-8";
+        };
+    };
+
+    config = {
+        networking = {
+            networkmanager.enable = true;
+            firewall.enable = true;
+        };
+
+        time.timeZone = config.custom.timeZone;
+        i18n = {
+            defaultLocale = config.custom.locale;
+            extraLocaleSettings = {
+                LC_ADDRESS = config.custom.extraLocale;
+                LC_IDENTIFICATION = config.custom.extraLocale;
+                LC_MEASUREMENT = config.custom.extraLocale;
+                LC_MONETARY = config.custom.extraLocale;
+                LC_NAME = config.custom.extraLocale;
+                LC_NUMERIC = config.custom.extraLocale;
+                LC_PAPER = config.custom.extraLocale;
+                LC_TELEPHONE = config.custom.extraLocale;
+                LC_TIME = config.custom.extraLocale;
+            };
+        };
+    };
+}
