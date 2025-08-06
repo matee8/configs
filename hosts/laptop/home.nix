3236f1f6efa04a571d091e841953114a5791e7d6
matee8 <graves-bluff-pesky@duck.com>
2025-08-06 17:55:33 +0200

    feat(hosts/laptop): add font configuration

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,4 +1,5 @@
 {
+    config,
     pkgs,
     ...
 }:
@@ -9,10 +10,32 @@
 
     stylix = {
         enable = true;
+        autoEnable = true;
+        polarity = "dark";
         base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
         targets.librewolf = {
             profileNames = [ "default" ];
             colorTheme.enable = true;
+        };
+
+        fonts = {
+            sizes = {
+                desktop = 14;
+                applications = 14;
+                terminal = 13;
+                popups = 13;
+            };
+            sansSerif = {
+                package = pkgs.nerd-fonts.hack;
+                name = "Hack Nerd Font";
+            };
+            serif = config.stylix.fonts.sansSerif;
+            monospace = config.stylix.fonts.sansSerif;
+            emoji = {
+                package = pkgs.noto-fonts-emoji;
+                name = "Noto Color Emoji";
+            };
         };
     };
 
